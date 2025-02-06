1635435d47cef011911ce4c11eabbb2553b7bd86
William English tus22 <146336377+WilliamEnglish@users.noreply.github.com>
2025-02-06 18:29:42 +0000

    Added zombie to follow player
    zombie also moves at a slightly slower speed then player

--- Assets/Scripts/Ai/Zombies.cs
+++ Assets/Scripts/Ai/Zombies.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Zombies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform Target; // The target the zombie will follow
+    public float UpdateSpeed = 0.1f; // How frequently to recalculate path based on Target's position
+
+    private NavMeshAgent Agent;
+
+    private void Awake()
     {
-        
+        Agent = GetComponent<NavMeshAgent>(); // Get the NavMeshAgent component
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
+        StartCoroutine(FollowTarget()); // Start the coroutine to follow the target
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds wait = new WaitForSeconds(UpdateSpeed); // Create a wait time based on UpdateSpeed
         
+        while (true) // Continuously follow the target
+        {
+            // Set the destination of the NavMeshAgent to the target's position
+            Agent.SetDestination(Target.position); 
+
+            yield return wait; // Wait for the specified time before the next calculation
+        }
     }
 }
