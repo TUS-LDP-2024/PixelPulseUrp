70b951fb84bbffc8e53436217fc804ace6336004
William English tus22 <146336377+WilliamEnglish@users.noreply.github.com>
2025-03-29 14:12:47 +0000

    menu bugs fixed and rounds sticking in build

--- Assets/Scripts/UI/MenuNavUI.cs
+++ Assets/Scripts/UI/MenuNavUI.cs
@@ -1,26 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MenuNavUI : MonoBehaviour
 {
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        // Unlock mouse for menus
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
+
     public void StartGame()
     {
         SceneManager.LoadScene("SweetTooth");
+        // Lock mouse for gameplay
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     public void SettingsMenu()
     {
         SceneManager.LoadScene("SettingsMenu");
+        // Unlock mouse for menus
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void EndGame()
     {
         Application.Quit();
+    }
+
+    // Add this to handle cases where scene is loaded directly
+    private void OnEnable()
+    {
+        // Check current scene name and set cursor accordingly
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == "SweetTooth") // Gameplay scene
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else // Any other scene (menus)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
